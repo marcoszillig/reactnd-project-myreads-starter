4e25282d213e7fc96e4c658097421027686f4182
Marcos Zillig <marcos.zillig@justdigital.com.br>
2019-02-06 09:28:42 -0200

    refactoring folder system

--- src/App.js
+++ src/App.js
@@ -1,50 +1,35 @@
 import React from 'react'
-import * as BooksAPI from './BooksAPI'
-import { Route, Link } from 'react-router-dom'
+import * as BooksAPI from './Utils/BooksAPI'
+import { Route} from 'react-router-dom'
 import './App.css'
-import ListBooks from './ListBooks'
-import AddBook from './AddBook'
+import ListBook from './Components/List_Book'
+import BookSearch from './Components/Book_Search'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    // showSearchPage: false
-    books: [],
-    shelf: ''
+    Books: []
   }
   componentDidMount() {
-    BooksAPI.getAll()
-    .then((books) => {
-      this.setState(() => ({
-        books
-      }))
+    this.fetch_books_details()
+  }
+
+  fetch_books_details = () => {
+    BooksAPI.getAll().then((books) => {
+      this.setState({Books: books})
     })
   }
+
+  update_books_details = (book, shelf) => {
+    BooksAPI.update(book, shelf).then(() => {
+      this.fetch_books_details()
+    })
+  }
+
   render() {
     return (
       <div className="app">
-        <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
-          </div>
-          <Route exact path='/' render={() => (
-            <ListBooks 
-              books={this.state.books}
-            />
-          )} />
-        </div>
-        <Route path='/search' rendercomponent={AddBook}/>
-        <div className="open-search">
-          <Link
-          to='/search'
-          className='add-book'
-          >Add a book</Link>
-        </div>
+        <Route exact path="/" render={() => (<ListBook books={this.state.Books} onChange={this.update_books_details}/>)}/>
+        <Route exact path="/search" render={({history}) => (<BookSearch onChange={this.update_books_details} myBooks={this.state.Books}/>)}/>
       </div>
     )
   }
